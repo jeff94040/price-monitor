eeafe13449cb4d9bcb4cd9839f553e60312c9bc3
Jeff Schmidt <jeff94040@gmail.com>
2023-06-03 21:09:12 -0700

    updates

--- index.js
+++ index.js
@@ -4,41 +4,58 @@ const { JSDOM } = jsdom;
 
 async function scrape(){
 
-  const url = new URL('https://www.lowes.com/pd/Whirlpool-24-5-cu-ft-4-Door-French-Door-Refrigerator-with-Ice-Maker-Fingerprint-Resistant-Stainless-Steel-ENERGY-STAR/1000257811')
+  const url = new URL('https://www.amazon.com/dp/B092PN2C3M/ref=syn_sd_onsite_desktop_0?ie=UTF8&psc=1&pd_rd_plhdr=t')
+  //const url = new URL('https://www.homedepot.com/p/Leviton-Decora-15-Amp-Single-Pole-Rocker-AC-Quiet-Light-Switch-White-10-Pack-M32-05601-2WM/202204204')
+  //const url = new URL('https://www.crutchfield.com/p_537OS237BG/Salamander-Designs-Chameleon-Collection-Oslo-237.html')
+  //const url = new URL('https://www.lowes.com/pd/Whirlpool-24-5-cu-ft-4-Door-French-Door-Refrigerator-with-Ice-Maker-Fingerprint-Resistant-Stainless-Steel-ENERGY-STAR/1000257811')
 
   const options = {
-    headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'}
+    headers: {
+      'Accept-Language': 'en-US,en;q=0.9'
+    }
   }
 
-	const html = await (await fetch(url, options)).text()
+  console.log('fetching...')
+  const http = await fetch(url, options)
+  console.log('converting response to text...')
+	const html = await http.text()
+  console.log('converted response to text...')
 
   const virtualConsole = new jsdom.VirtualConsole();
   const dom = new JSDOM(html, { virtualConsole });
   const document = dom.window.document
-  //console.log(document)
+  //console.log(document.body.outerHTML)
 
   var elem
 
   switch(url.hostname) {
 
     case 'www.amazon.com':
-      elem = document.querySelector(".twister-plus-buying-options-price-data")
+      elem = document.querySelector(".a-offscreen")
       if (elem != null)
-        console.log(JSON.parse(elem.textContent)[0].priceAmount)
+        console.log(elem.textContent)
       else
         console.log('elem not found in html document')
       break
       
     case 'www.homedepot.com':
-      elem = document.querySelector("#thd-helmet__script--productStructureData")
+      elem = document.querySelector(".price-format__large.price-format__main-price")
       if (elem != null)
-        console.log(JSON.parse(elem.textContent).offers.price)
+        console.log(elem.textContent)
       else
-        console.log('elem not found in html document')
+        console.log('elem not found in home depot document')
       break
     
+    case 'www.crutchfield.com':
+      elem = document.querySelector(".price.js-price")
+      if (elem != null)
+        console.log(elem.textContent.trim())
+      else
+      console.log('elem not found in html document')
+      break
+
     case 'www.lowes.com':
-      elem = document.querySelector(".screen-reader")
+      elem = document.querySelector("#bundled-carousel-better-together-atc-333346-44211-L51844481000842744")
       if (elem != null)
         console.log(elem.textContent)
       else
